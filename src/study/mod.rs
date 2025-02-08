736b58c038541c8121b246bf30d8cf8f55bdb6d3
Matéo Fernandez <mateo.fernandez@etu.umontpellier.fr>
2025-02-08 19:02:24 +0100

    refactor: merge the two enums

--- src/study/mod.rs
+++ src/study/mod.rs
@@ -2,17 +2,15 @@ use kana::Kana;
 use rand::seq::SliceRandom;
 use strum::IntoEnumIterator;
 
-pub mod hiragana;
 pub mod kana;
-pub mod katakana;
 
 pub trait ValidateGuess {
-    fn validate(&self, guess: &str) -> bool;
+    fn validate_guess(&self, guess: &str) -> bool;
 }
 
-pub fn create_full_study_plan() -> Vec<Kana> {
-    let mut all_kana: Vec<Kana> = Kana::iter().collect();
-    all_kana.shuffle(&mut rand::rng());
+pub fn create_study_plan() -> Vec<Kana> {
+    let mut kanas: Vec<Kana> = Kana::iter().collect();
+    kanas.shuffle(&mut rand::rng());
 
-    all_kana
+    kanas
 }
