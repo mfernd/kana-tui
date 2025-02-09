8ccd7be0679ae90b4cb78d6c468db061a57b171d
Matéo Fernandez <mateo.fernandez73@gmail.com>
2025-02-09 17:36:31 +0100

    chore(clippy): silence dead code for now

--- src/study/mod.rs
+++ src/study/mod.rs
@@ -4,10 +4,12 @@ use strum::IntoEnumIterator;
 
 pub mod kana;
 
+#[allow(dead_code)]
 pub trait ValidateGuess {
     fn validate_guess(&self, guess: &str) -> bool;
 }
 
+#[allow(dead_code)]
 pub fn create_study_plan() -> Vec<Kana> {
     let mut kanas: Vec<Kana> = Kana::iter().collect();
     kanas.shuffle(&mut rand::rng());
